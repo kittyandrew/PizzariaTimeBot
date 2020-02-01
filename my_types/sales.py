c393cd432ae08d01e4f6b95220f699e5fb6a034e
andrebcd4 <andrebc375@gmail.com>
2020-02-01 03:20:04 +0200

    new Discount class, save/load in Sales class

--- my_types/sales.py
+++ my_types/sales.py
@@ -1,5 +1,31 @@
+from typing import List
+import datetime
+import pickle
+import os
 
 
 class BotSales:
+    PATH = "discounts.pickle"
     def __init__(self):
-        pass
+        self.load_instance()
+
+    def load_instance(self):
+        if os.path.exists(self.PATH):
+            with open(self.PATH, "rb") as _file:
+                self.discounts = pickle.load(_file)
+        else:
+            self.discounts = []
+            self.save()
+
+    def save(self):
+        with open(self.PATH, "wb") as _file:
+            pickle.dump(self.discounts, _file)
+
+
+class Discount:
+    def __init__(self):
+        self.text = ""
+        self.hours = ""
+        self._hours:List[datetime.datetime] = None
+        self.everyday = True
+        self.repetative = True
